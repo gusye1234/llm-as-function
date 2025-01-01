db6494bb8adf8d398e153f404e1a355c3271e840
Turtle <erencemayez@gmail.com>
2025-01-01 14:47:15 +0300

    added scaffold ollama functions to models

--- llm_as_function/models.py
+++ llm_as_function/models.py
@@ -14,6 +14,11 @@ JSON_SCHEMA_PROMPT = {
 !!! You must output your results in the following JSON schema. Strictly adhere to the following JSON schema, and do not return a list in this format:
 {json_schema}
 """,
+    "ollama": """
+
+!!! You must output your results in the following JSON schema. Strictly adhere to the following JSON schema, and do not return a list in this format:
+{json_schema}
+"""
 }
 
 
@@ -59,6 +64,14 @@ def async_openai_max_retry(func, retry_times=3):
     return new_func
 
 
+def ollama_max_retry(func, retry_times=3):
+    raise NotImplementedError
+
+
+def async_ollama_max_retry(func, retry_times=3):
+    raise NotImplementedError
+
+
 @openai_max_retry
 def openai_single_create(
     query,
@@ -96,3 +109,27 @@ async def openai_single_acreate(
         **runtime_options,
     )
     return response
+
+
+@ollama_max_retry
+def ollama_single_create(
+    query,
+    client: OpenAI,
+    model="gpt-3.5-turbo-1106",
+    temperature=0.1,
+    function_messages=[],
+    runtime_options={},
+):
+    raise NotImplementedError
+
+
+@async_ollama_max_retry
+async def ollama_single_acreate(
+    query,
+    client: AsyncOpenAI,
+    model="gpt-3.5-turbo-1106",
+    temperature=0.1,
+    function_messages=[],
+    runtime_options={},
+):
+    raise NotImplementedError
