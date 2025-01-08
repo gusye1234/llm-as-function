2556e9e255b1d91589310a2d9073eed839895e42
Turtle <erencemayez@gmail.com>
2025-01-08 12:55:48 +0300

    fix: Removed assertion so only a warning is given when opanai key isn't present

--- llm_as_function/__init__.py
+++ llm_as_function/__init__.py
@@ -1,10 +1,11 @@
 from .llm_func import LLMFunc, Final
 import os
 
-if "OPENAI_API_KEY" in os.environ:
-    gpt35_func = LLMFunc(temperature=0.1, has_tool_support=True)
-    gpt4_func = LLMFunc(temperature=0.1, model="gpt-4o", has_tool_support=True)
+# OpenAI LLMFuncs
+gpt35_func = LLMFunc(temperature=0.1, has_tool_support=True)
+gpt4_func = LLMFunc(temperature=0.1, model="gpt-4o", has_tool_support=True)
 
+# Ollama LLMFuncs
 llama2_func = LLMFunc(temperature=0.1, model="llama2", has_structured_output=True)
 llama3_func = LLMFunc(temperature=0.1, model="llama3", has_structured_output=True)
 llama3_1_func = LLMFunc(temperature=0.1, model="llama3.1", has_tool_support=True, has_structured_output=True)
